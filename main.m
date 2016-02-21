4ebd23cbd64492517711e1004c2878379390176d
VassilyChi <cjfire@sina.cn>
2016-02-21 20:25:58 +0800

    两步

--- main.m
+++ main.m
@@ -1,1 +1,3 @@
 // mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes; cd ~/Library/Developer/Xcode/UserData/FontAndColorThemes; curl -O http://developers.enormego.com/assets/egotheme/EGOv2.dvtcolortheme
+
+RK-64WW-MEJ6-F8G8
