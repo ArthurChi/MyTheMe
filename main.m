bd4d4d895f70743eca3192b21f7491e108b097f7
VassilyChi <cjfire@sina.cn>
2016-02-22 09:18:37 +0800

    change

--- main.m
+++ main.m
@@ -1,3 +1,3 @@
 // mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes; cd ~/Library/Developer/Xcode/UserData/FontAndColorThemes; curl -O http://developers.enormego.com/assets/egotheme/EGOv2.dvtcolortheme
 
-RK-64WW-MEJ6-F8G8
+4cb4bf2fa19ba684e41fdccdc85ede630746e6d7b4f509dfe9a8b2075bbe9230c3d087a06c8668ee57f99d54237b823b2dae6aa1d4ecd87daaf4b026c8547ae1d8e16d43b075e7b99b9bf29e711dc708
